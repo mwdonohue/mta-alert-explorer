16835484d6d52daefa40ebe09af4510e6afc9354
Mikey Donohue <mwdonohue1234@gmail.com>
2024-01-13 17:40:41 -0500

    Replaced fetch call with axios

--- db/src/services/mtaService.ts
+++ db/src/services/mtaService.ts
@@ -1,11 +1,11 @@
+import axios from 'axios'
+
 export async function getMTAData(): Promise<any> {
-  const resp = await fetch(
-    'https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json',
-    {
+  return axios
+    .get('https://api-endpoint.mta.info/Dataservice/mtagtfsfeeds/camsys%2Fsubway-alerts.json', {
       headers: {
         'x-api-key': process.env.MTA_API_KEY,
       },
-    },
-  )
-  return await resp.json()
+    })
+    .then((resp) => resp.data)
 }
