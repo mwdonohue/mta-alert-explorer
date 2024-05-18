04905cdd7cbb82d47f3d80a828b26d5824eb1fe2
Mikey Donohue <startress137911@gmail.com>
2024-05-18 19:37:36 -0400

    Replaced API with SB skeleton

---
+++ api/src/main/java/io/github/mwdonohue/api/API.java
@@ -0,0 +1,13 @@
+package io.github.mwdonohue.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class API {
+
+    public static void main(String[] args) {
+        SpringApplication.run(API.class, args);
+    }
+
+}
